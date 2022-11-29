0fb2c2e1c3b608fed7e6ddef42f81fd44bcffafc
dmitri_zavadski <>
2022-11-29 15:55:26 +0300

    Add panfsPath attribute to the mb command

--- api-put-bucket.go
+++ api-put-bucket.go
@@ -33,16 +33,16 @@ func (c *Client) makeBucket(ctx context.Context, bucketName string, opts MakeBuc
 		return err
 	}
 
-	err = c.doMakeBucket(ctx, bucketName, opts.Region, opts.ObjectLocking)
+	err = c.doMakeBucket(ctx, bucketName, opts.Region, opts.PanfsPath, opts.ObjectLocking)
 	if err != nil && (opts.Region == "" || opts.Region == "us-east-1") {
 		if resp, ok := err.(ErrorResponse); ok && resp.Code == "AuthorizationHeaderMalformed" && resp.Region != "" {
-			err = c.doMakeBucket(ctx, bucketName, resp.Region, opts.ObjectLocking)
+			err = c.doMakeBucket(ctx, bucketName, resp.Region, opts.PanfsPath, opts.ObjectLocking)
 		}
 	}
 	return err
 }
 
-func (c *Client) doMakeBucket(ctx context.Context, bucketName string, location string, objectLockEnabled bool) (err error) {
+func (c *Client) doMakeBucket(ctx context.Context, bucketName string, location string, panfsPath string, objectLockEnabled bool) (err error) {
 	defer func() {
 		// Save the location into cache on a successful makeBucket response.
 		if err == nil {
@@ -68,6 +68,12 @@ func (c *Client) doMakeBucket(ctx context.Context, bucketName string, location s
 	if objectLockEnabled {
 		headers := make(http.Header)
 		headers.Add("x-amz-bucket-object-lock-enabled", "true")
+		reqMetadata.customHeader = headers
+	}
+
+	if panfsPath != "" {
+		headers := make(http.Header)
+		headers.Add("x-panfs-bucket-path", panfsPath)
 		reqMetadata.customHeader = headers
 	}
 
@@ -106,7 +112,8 @@ func (c *Client) doMakeBucket(ctx context.Context, bucketName string, location s
 // MakeBucketOptions holds all options to tweak bucket creation
 type MakeBucketOptions struct {
 	// Bucket location
-	Region string
+	Region    string
+	PanfsPath string
 	// Enable object locking
 	ObjectLocking bool
 }
